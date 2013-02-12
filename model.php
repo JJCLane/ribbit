530fc5c9d8e13b597bdf6ccd8c924fd09c2bb8bb
Jordan Lane <jordanlane@hotmail.co.uk>
2013-02-12 22:13:01 +0000

    Added getUserInfo and getFollowersRibbits functions

--- model.php
+++ model.php
@@ -109,5 +109,30 @@
 			$this->delete("UserAuth", array("hash" => $hash));
 			setcookie ("Auth", "", time() - 3600);
 		}
+		public function getUserInfo($user)
+		{
+			$query = "SELECT ribbit_count, IF(ribbit IS NULL, 'You have no Ribbits', ribbit) as ribbit, followers, following ";
+			$query .= "FROM (SELECT COUNT(*) AS ribbit_count FROM Ribbits WHERE user_id = " . $user->id . ") AS RC ";
+			$query .= "LEFT JOIN (SELECT user_id, ribbit FROM Ribbits WHERE user_id = " . $user->id . " ORDER BY created_at DESC LIMIT 1) AS R ";
+			$query .= "ON R.user_id = " . $user->id . " JOIN ( SELECT COUNT(*) AS followers FROM Follows WHERE followee_id = " . $user->id;
+			$query .=  ") AS FE JOIN (SELECT COUNT(*) AS following FROM Follows WHERE user_id = " . $user->id . ") AS FR;";
+			$res = $this->db->query($query);
+			return $res->fetch_object();
+		}
+		public function getFollowersRibbits($user)
+		{
+			$query = "SELECT name, username, gravatar_hash, ribbit, Ribbits.created_at FROM Ribbits JOIN (";
+			$query .= "SELECT Users.* FROM Users LEFT JOIN (SELECT followee_id FROM Follows WHERE user_id = ";
+			$query .= $user->id . " ) AS Follows ON followee_id = id WHERE followee_id = id OR id = " . $user->id;
+			$query .= ") AS Users on user_id = Users.id ORDER BY Ribbits.created_at DESC LIMIT 10;";
+			$res = $this->db->query($query);
+			$fribbits = array();
+			while($row = $res->fetch_object())
+			{
+				array_push($fribbits, $row);
+			}
+			return $fribbits;
+		}
+
 	}
 ?>
