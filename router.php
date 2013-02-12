68a29106240444384d626e62aea9b1962dca4487
Jordan Lane <jordanlane@hotmail.co.uk>
2013-02-12 17:45:00 +0000

    Add router class and lookup method

--- router.php
+++ router.php
@@ -1,57 +1,31 @@
 <?php
-	class Model{
-		private $db; //Holds mysqli variable
+	class Router{
+		private $routes;
 
 		function __construct(){
-			$this->db = new mysqli('localhost', 'root', '', 'ribbit');
+			$this->routes = array(
+				"home" => "indexPage",
+        		"signup" => "signUp",
+        		"login" => "login",
+        		"buddies" => "buddies",
+        		"ribbit" => "newRibbit",
+        		"logout" => "logout",
+        		"public" => "publicPage",
+        		"profiles" => "profiles",
+        		"unfollow" => "unfollow",
+        		"follow" => "follow";
+        	);
 		}
-		//--- private function for performing standard INSERTs
-		private function select($table, $arr){
-			$query = "SELECT * FROM " . $table;
-			$pref = " WHERE ";
-			foreach ($arr as $key => $value)
-			{
-				$query .= $pref . $key . "='" . $value . "'";
-				$pref = " AND ";
-			}
-			$query .= ";";
-			return $this->db->query($query);
-		}
-		private function insert($table, $arr)
+		//--- public function for performing lookup
+		public function lookup($query)
 		{
-		    $query = "INSERT INTO " . $table . " (";
-		    $pref = "";
-		    foreach($arr as $key => $value)
-		    {
-		        $query .= $pref . $key;
-		        $pref = ", ";
-		    }
-		    $query .= ") VALUES (";
-		    $pref = "";
-		    foreach($arr as $key => $value)
-		    {
-		        $query .= $pref . "'" . $value . "'";
-		        $pref = ", ";
-		    }
-		    $query .= ");";
-		    return $this->db->query($query);
-		}
-		//--- private function for performing standard DELETEs
-		private function delete($table, $arr){
-		    $query = "DELETE FROM " . $table;
-		    $pref = " WHERE ";
-		    foreach($arr as $key => $value)
-		    {
-		        $query .= $pref . $key . "='" . $value . "'";
-		        $pref = " AND ";
-		    }
-		    $query .= ";";
-		    return $this->db->query($query);
-		}
-		//--- private function for checking if a row exists
-		private function exists($table, $arr){
-		    $res = $this->select($table, $arr);
-		    return ($res->num_rows > 0) ? true : false;
+			if(array_key_exists($query, $this->routes))
+			{
+				return $this->routes[$query];
+			} else 
+			{
+				return false;
+			}
 		}
 	}
 ?>
